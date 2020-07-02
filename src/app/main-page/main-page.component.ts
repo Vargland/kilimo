be034888cea9be00ec8fdb486435deaaae11f966
German <psi.germanr@gmail.com>
2020-07-02 00:38:20 -0300

    kilimo/destroyed subscriptions

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,8 +1,8 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnInit, Output, OnDestroy } from '@angular/core';
 import { IUserData } from '../mock-data'
 import { MainPageService } from './main-page.component.service'
-import { filter, map } from 'rxjs/operators';
-import { Observable, BehaviorSubject } from 'rxjs';
+import { filter, tap, takeUntil } from 'rxjs/operators';
+import { Observable, BehaviorSubject, Subject } from 'rxjs';
 
 @Component({
   selector: 'main-page-component',
@@ -10,11 +10,12 @@ import { Observable, BehaviorSubject } from 'rxjs';
   styleUrls: ['./main-page.component.scss']
 })
 
-export class MainPageComponent implements OnInit {
+export class MainPageComponent implements OnInit, OnDestroy {
   @Output() public dataUser: Observable<IUserData[]>
   @Output() public displayedColumns: string[];
 
   private _userData$: BehaviorSubject<IUserData[]> = new BehaviorSubject(undefined)
+  private _destroyed$ = new Subject<void>();
 
   constructor(private _mainPageService: MainPageService) { 
     this._mainPageService.getUserData();
@@ -26,7 +27,12 @@ export class MainPageComponent implements OnInit {
     this._mainPageService.userData$
       .pipe(
         filter((userData) => userData !== undefined),
-        map((userData: IUserData[]) => this._userData$.next(userData))
+        tap((userData: IUserData[]) => this._userData$.next(userData)),
+        takeUntil(this._destroyed$),
       ).subscribe();
+  }
+
+  public ngOnDestroy() {
+    this._destroyed$.next();
   }
 }
